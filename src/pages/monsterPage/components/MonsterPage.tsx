8ac59363ce73e85780df98fc9d98d28bc0002c84
adamkali <adamkali@outlook.com>
2022-02-14 08:38:50 -0500

    Merge

--- src/pages/monsterPage/components/MonsterPage.tsx
+++ src/pages/monsterPage/components/MonsterPage.tsx
@@ -1,18 +1,16 @@
-import Monster from "../../../data/monsters.json";
+import monster from "../../../data/monsters.json";
+import Monster from "../../../classes/Monster";
 
-export default function MonsterPage(): JSX.Element {
-    return (
-        <div className="card">
-            <div className="card-header">
+export default function MonsterPage(monster: Monster): JSX.Element {
+	return (
+		<div className="card">
+			<div className="card-header">
 				<div style={{ padding: "5px" }}>
-					<label
-						className="display-4"
-						style={{ padding: "10px 10px 10px 0" }}
-					>
+					<label className="display-4" style={{ padding: "10px 10px 10px 0" }}>
 						<span>Monster manual</span>
 					</label>
-                </div>
-            </div>
-        </div>
-    );
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/pages/monsterPage/index.tsx
+++ src/pages/monsterPage/index.tsx
@@ -1,32 +1,30 @@
 import { useState, useEffect } from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    useLocation,
-    useParams,
+	BrowserRouter as Router,
+	useLocation,
+	useParams,
 } from "react-router-dom";
 import monsterManual from "../../data/monsters.json";
 import Monster from "../../classes/Monster";
-import MonsterPage from "components/MonsterPage";
+import MonsterPage from "./components/MonsterPage";
 
 export default function MonsterDisplay(): JSX.Element {
-    const params = useParams();
-    const [monster, setMonster] = useState({} as Monster);
-    const [monsters, setMonsters] = useState(
-        monsterManual as Monster[]
-    );
+	const params = useParams();
+	const [monster, setMonster] = useState({} as Monster);
+	const [monsters, setMonsters] = useState(monsterManual as Monster[]);
 
-    useEffect(() => {
-        monsters.forEach((mon) => {
-            if (mon.id === params.id) {
-                setMonster(mon);
-            }
-        }); 
-    }, [params]);
+	useEffect(() => {
+		let id = params.id ?? "";
+		monsters.forEach((mon) => {
+			if (mon.id === +id) {
+				setMonster(mon);
+			}
+		});
+	}, [params]);
 
-    return (
-        <div>
-            <MonsterPage monster={monster}/>
-        </div>
-    );
+	return (
+		<div>
+			<MonsterPage monster={monster} />
+		</div>
+	);
 }
